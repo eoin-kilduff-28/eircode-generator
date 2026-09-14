008c665e3a3262f8831b17d66c9713ca66badcff
blanknamedotcom <eoinkil1405@gmail.com>
2026-09-14 16:33:23 +0100

    Adding in a small array of towns for each county, 3-4 each. Since the  Eircode database is not accessible, I will have to generate random Eircodes and place them in my own database as a makeshift Eircode database.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const county = document.querySelector("#counties").value;
+const county = document.querySelector("#counties");
+const townContainer = document.querySelector("#town-container");
+const towns = document.querySelector("#towns");
 
  county.addEventListener("change",
     function(){
-        const county = this.value;
-        console.log(county);
+        const chosenCounty = this.value;
+        console.log(chosenCounty);
 
     }
- )
+     );
+
+    const countyTowns = {
+        CW: ["Carlow Town", "Tullow", "Muine Bheag"],
+        CN: ["Cavan Town", "Kingscourt", "Bailieborough"],
+        CE: ["Ennis", "Shannon", "Kilrush"],
+        CK: ["Cobh", "Cork City", "Carrigaline", "Mallow"],
+    }
